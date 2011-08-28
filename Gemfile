0a2b56a4c6ae6ab47768ec7ea0731596daeb161a
openecho <j.w.marsden@gmail.com>
2011-08-28 12:58:41 +0930

    Include RedCloth - port pages to Textile

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.0.1'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
-
+gem 'RedCloth', '4.2.2'
 
 group :development do
    gem 'rspec-rails', '2.0.1'
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,10 @@
 module PagesHelper
+	
+	def content_block(text)
+		raw("<div class='pad'>\r\n" + raw_textile(text) + "\r\n</div>\r\n")
+	end
+		
+	def raw_textile(text)
+		raw(RedCloth.new(text, [:no_span_caps]).to_html)
+	end
 end
